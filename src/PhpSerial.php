3599ccb9b70d313bc0f38546cab7860dd6ad938b
Luis Escobar Bravo <luisescobarbravo@gmail.com>
2018-04-11 11:41:10 -0400

    Updated constructor name

--- src/PhpSerial.php
+++ src/PhpSerial.php
@@ -16,7 +16,7 @@ define ("SERIAL_DEVICE_OPENED", 2);
  * @thanks Jim Wright for OSX cleanup/fixes.
  * @copyright under GPL 2 licence
  */
-class PhpSerial
+class __construct
 {
     public $_device = null;
     public $_winDevice = null;
